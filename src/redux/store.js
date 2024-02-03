5ee6230ca1d72f712d55d2e33fe91d6750a9311c
ira089 <gvir2015@gmail.com>
2024-02-03 18:28:02 +0200

    react_HW6_redux_LS

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
-import contactReducer from './contactClice';
-import filterReducer from './filterSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+// import contactReducer from './contactClice';
+// import filterReducer from './filterSlice';
+import { reducer } from './reducer';
 
-const store = configureStore({
-  reducer: {
-    contacts: contactReducer,
-    filter: filterReducer,
-  },
-});
+// export const store = configureStore({
+//   reducer: {
+//     contacts: contactReducer,
+//     filter: filterReducer,
+//   },
+// });
+// вариант Ломач
+const persistConfig = {
+  key: 'contactsLS',
+  storage,
+  whitelist: ['contacts'],
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, reducer);
+export const store = configureStore({ reducer: persistedReducer });
+
+export const persistor = persistStore(store);
